dac5edcdffdd648543fead43c789eb1efcd0a699
Matt Driscoll <MDriscoll@esri.com>
2020-07-14 12:31:23 -0700

    repro case

--- stencil.config.ts
+++ stencil.config.ts
@@ -1,19 +1,23 @@
-import { Config } from '@stencil/core';
+import { Config } from "@stencil/core";
 
 export const config: Config = {
-  namespace: 'stencil-starter-project-name',
-  taskQueue: 'async',
+  namespace: "stencil-starter-project-name",
+  taskQueue: "async",
+  hydratedFlag: {
+    selector: "attribute",
+    name: "calcite-hydrated",
+  },
   outputTargets: [
     {
-      type: 'dist',
-      esmLoaderPath: '../loader'
+      type: "dist",
+      esmLoaderPath: "../loader",
     },
     {
-      type: 'docs-readme'
+      type: "docs-readme",
     },
     {
-      type: 'www',
-      serviceWorker: null // disable service workers
-    }
-  ]
+      type: "www",
+      serviceWorker: null, // disable service workers
+    },
+  ],
 };
